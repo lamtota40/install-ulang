32383b67c3ae2a40df6029b434fef38dc7d74b16
lamtota40 <lamtota40@gmail.com>
2025-05-12 16:01:26 +0700

    Update grub.sh

--- grub.sh
+++ grub.sh
@@ -1,10 +1,21 @@
-sudo nano /etc/grub.d/40_custom
-menuentry "Restore Snapshot (Btrfs Clean Boot)" {
+sudo bash -c 'cat << EOF >> /etc/grub.d/40_custom
+menuentry "restore snapshot btrfs" {
     insmod btrfs
     insmod part_gpt
     insmod ext2
-    set root='hd0,gpt1'  # Ganti sesuai lokasi partisi kamu
-    linux /boot/vmlinuz-$(uname -r) root=UUID=$(blkid -s UUID -o value /dev/sda1) rootflags=subvolid=5 ro quiet
-    initrd /boot/initrd.img-$(uname -r)
+    set root="hd0,gpt1"
+    linux /boot/vmlinuz-\$(uname -r) root=UUID=\$(blkid -s UUID -o value /dev/sda1) rootflags=subvolid=5 ro quiet
+    initrd /boot/initrd.img-\$(uname -r)
 }
+EOF'
 sudo update-grub
+sudo reboot-grub "restore snapshot btrfs"
+
+sudo tee /etc/rc.local > /dev/null <<EOF
+#!/bin/bash
+/usr/local/bin/btrfs-restore.sh
+exit 0
+EOF
+
+sudo chmod +x /etc/rc.local
+sudo systemctl enable rc-local
