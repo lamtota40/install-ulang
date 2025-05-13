e049e511edd3a2ed4315ea43d07b40d9accd8684
lamtota40 <lamtota40@gmail.com>
2025-05-13 17:03:40 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -8,19 +8,20 @@ sudo btrfs subvolume delete /mnt/tmp
 sudo cp /etc/fstab /etc/fstab.bak
 sudo sed -i '/^[^#]*[[:space:]]\/home[[:space:]]\+btrfs.*subvol=@home/d' /etc/fstab
 sudo reboot
-
+####################################################
+# Backup snapshoot btfrs
 #mount
 sudo mount /dev/sda1 /mnt
 #melihat list btrfs
 sudo btrfs subvolume list /mnt
-
 #backup
 sudo btrfs subvolume snapshot -r /mnt/@ /mnt/@_backup
 sudo btrfs send /mnt/@_backup | gzip -c > btrfs-sda1-backup.img.gz
-
 # bersihkan
 sudo btrfs subvolume delete /mnt/@_backup
-
+# catat size semua partisi dalam byte agar presisi
+sudo parted /dev/sda unit B print
+####################################################
 # Membuat partisi & Restore dari GRML toram (Live CD)
 sudo parted /dev/sdb
 (parted) mklabel msdos
@@ -33,6 +34,7 @@ sudo parted /dev/sdb
 sudo mkfs.btrfs /dev/sdb1
 sudo mkfs.btrfs /dev/sdb2
 
+#mount dan restore
 sudo mount /dev/sda1 /mnt
 sudo gunzip -c btrfs-sda1-backup.img.gz | sudo btrfs receive /mnt
 
