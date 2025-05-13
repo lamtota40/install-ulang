52fafddf83f9e753c14ccdb5e69c3176d48da043
lamtota40 <lamtota40@gmail.com>
2025-05-13 15:48:19 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -18,8 +18,8 @@ sudo btrfs subvolume list /mnt
 sudo btrfs subvolume snapshot -r /mnt/@ /mnt/@_backup
 sudo btrfs send /mnt/@_backup | gzip -c > btrfs-root-backup.img.gz
 
-sudo btrfs subvolume snapshot -r /mnt/@home /mnt/@home_backup
-sudo btrfs send /mnt/@home_backup | gzip -c > btrfs-home-backup.img.gz
+#sudo btrfs subvolume snapshot -r /mnt/@home /mnt/@home_backup
+#sudo btrfs send /mnt/@home_backup | gzip -c > btrfs-home-backup.img.gz
 
 # bersihkan
 sudo btrfs subvolume delete /mnt/@_backup
