60a3b729da7b18ac65dc078a01b434d2dd1de87e
lamtota40 <lamtota40@gmail.com>
2025-05-17 10:05:58 +0700

    Update and rename vps-freeze to vps-freeze.sh

--- vps-freeze.sh
+++ vps-freeze.sh
@@ -8,12 +8,14 @@ sudo umount /mnt/btrfs_root
 
 #restore dari GRML
 #!/bin/bash
-
-sudo mount -o subvolid=5 /dev/vda3 /mnt
-cd /mnt
+sudo mkdir -p /mnt/btrfs_root
+sudo mount -o subvolid=5 /dev/vda3 /btrfs_root
+cd /btrfs_root
 sudo btrfs subvolume delete @
 sudo btrfs subvolume snapshot @clean @
 sudo btrfs subvolume delete @clean
 cd
+sudo umount /mnt/btrfs_root
+
 sync
 reboot
