a82fea6f230a5aca4637810824c33c30e6e0a91c
lamtota40 <lamtota40@gmail.com>
2025-05-17 17:12:29 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -20,7 +20,8 @@ sudo mkdir /mnt/root
 sudo mount /dev/vda3 /mnt/root
 
 #dari pengirim
-sudo rsync -avz -e ssh /mnt/usb/btrfs-sda1-backup.img.gz root@147.139.143.79:/mnt/vda4
+ssh-keygen -f "~/.ssh/known_hosts" -R "147.139.143.79"
+sudo rsync -avz --info=progress2 -e ssh /mnt/usb/btrfs-sda1-backup.img.gz root@147.139.143.79:/mnt/vda4
 
 #estrak dan di terima btfrs
 sudo gunzip -c /mnt/vda4/btrfs-sda1-backup.img.gz | sudo btrfs receive /mnt/root
