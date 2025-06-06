d37bd731e38d29067c25b795ba8011c8563dafec
lamtota40 <lamtota40@gmail.com>
2025-06-06 21:14:39 +0700

    Update autorun-grml.sh

--- autorun-grml.sh
+++ autorun-grml.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-ls
+# Ambil waktu saat ini dalam format menit-detik
+prefix=$(date +'%M-%S')
+counter=1
+
+# Cari nama file yang belum ada
+while [[ -e "${prefix}-${counter}.txt" ]]; do
+    ((counter++))
+done
+
+# Buat file baru dengan nama yang belum ada
+touch "${prefix}-${counter}.txt"
