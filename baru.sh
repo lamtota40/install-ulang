8cb7e674cda63fedee67b8009d4bc9b901e7ae77
lamtota40 <lamtota40@gmail.com>
2025-06-07 19:01:22 +0700

    Update baru.sh

--- baru.sh
+++ baru.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 sudo apt update
 sudo apt upgrade -y
-sudo
+sudo apt install zsh openssh-server -y
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sudo sed -i '/^#\?PermitRootLogin/c\PermitRootLogin yes' /etc/ssh/sshd_config
+sudo systemctl restart ssh
