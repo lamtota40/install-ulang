a0366cfaeabcefb3fdb385cf4c6617f25f729447
lamtota40 <lamtota40@gmail.com>
2025-06-09 16:49:38 +0700

    Update snapstore.sh

--- snapstore.sh
+++ snapstore.sh
@@ -13,7 +13,7 @@ sudo btrfs send /mnt/btrfs/@_backup | gzip -c > btrfs-backup.img.gz
 #backup tanpa kompresi
 sudo btrfs send /mnt/btrfs/@_backup > btrfs-backup.img
 #backup ke partisi lain misal /dev/sda2
-sudo btrfs send /mnt/btrfs/@_backup > /mnt/sda2/btrfs-sda1-backup.img
+sudo btrfs send /mnt/btrfs/@_backup > /mnt/sda2/btrfs-backup.img
 sudo btrfs send /mnt/btrfs/@_backup | gzip -c > /mnt/sda2/btrfs-sda1-backup.img.gz
 
 
