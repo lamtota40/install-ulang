1be8ed22fe18a9ccfc522261c1dea29fd823285e
lamtota40 <lamtota40@gmail.com>
2025-06-09 23:47:40 +0700

    Update coba.sh

--- coba.sh
+++ coba.sh
@@ -14,6 +14,7 @@ dpkg -i debootstrap_1.0.141_all.deb
 which debootstrap && debootstrap --version
 debootstrap bionic /mnt http://archive.ubuntu.com/ubuntu/
 
+cat /mnt/etc/resolv.conf
 rm -rf /mnt/etc/resolv.conf
 #cp /etc/resolv.conf /mnt/etc/resolv.conf
 echo -e "nameserver 8.8.8.8\nnameserver 1.1.1.1" > /etc/resolv.conf
