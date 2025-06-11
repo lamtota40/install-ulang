b9f633f3eb6c8c2052901aa497f3180fe3556fdb
lamtota40 <lamtota40@gmail.com>
2025-06-11 13:54:52 +0700

    Update install-via-debootstrap.sh

--- install-via-debootstrap.sh
+++ install-via-debootstrap.sh
@@ -27,6 +27,8 @@ mount --bind /proc /mnt/proc
 mount --bind /sys /mnt/sys
 cat <<'EOL' | chroot /mnt /bin/bash
 mkdir -p /root
+chmod 700 /root
+chown root:root /root
 cat > /etc/apt/sources.list <<'EOF'
 deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse
 deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse
