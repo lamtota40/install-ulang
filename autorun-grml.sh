9fc0e14902095a4cef145cad2fff518c6a660656
lamtota40 <lamtota40@gmail.com>
2025-06-13 18:17:04 +0700

    Update autorun-grml.sh

--- autorun-grml.sh
+++ autorun-grml.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ls
+sudo mkdir -p /mnt/res
+sudo mount -o subvolid=5 /dev/vda1 /mnt/res
+sudo btrfs subvolume list /mnt/res
+sudo btrfs subvolume delete /mnt/res/@
+sudo btrfs subvolume snapshot /mnt/res/@_clean /mnt/res/@
+sudo btrfs subvolume list /mnt/res
+sudo umount /mnt/res
+sudo rm -rf /mnt/res
+sync
+sudo reboot
