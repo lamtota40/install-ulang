2a12bb306c12dec8d086cd9a8c4158edb9a9aa3a
lamtota40 <lamtota40@gmail.com>
2025-06-14 09:36:17 +0700

    Update autorun-grml.sh

--- autorun-grml.sh
+++ autorun-grml.sh
@@ -5,8 +5,8 @@ sudo mount -o subvolid=5 /dev/vda1 /mnt/res
 sudo btrfs subvolume list /mnt/res
 sudo btrfs subvolume delete /mnt/res/@
 sudo btrfs subvolume snapshot /mnt/res/@_clean /mnt/res/@
-ID=$(btrfs subvolume show /mnt/res 2>&1 | awk -F': *' '/Subvolume ID:/ {print $2}' | xargs)
-btrfs subvolume set-default "$ID" /mnt/res
+ID=$(sudo btrfs subvolume show /mnt/res 2>&1 | awk -F': *' '/Subvolume ID:/ {print $2}' | xargs)
+sudo btrfs subvolume set-default "$ID" /mnt/res
 sudo umount /mnt/res
 sudo rm -rf /mnt/res
 sync
